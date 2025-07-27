d3697d47df6f5d90485ad91112bed560af918118
Codix9000 <riveraesteban.ch@gmail.com>
2025-07-27 14:43:13 +0200

    URL sitemape.ts and turb.json

--- apps/web/app/sitemap.ts
+++ apps/web/app/sitemap.ts
@@ -1,42 +1,46 @@
 import { MetadataRoute } from "next";
 import { api } from "../convex/_generated/api";
-import { ConvexHttpClient } from "convex/browser";
+import { ConvexClient } from "convex/browser";
 
-const client = new ConvexHttpClient(
-  process.env["NEXT_PUBLIC_CONVEX_URL"] as string,
+const client = new ConvexClient(
+  process.env["CONVEX_DEPLOYMENT"] as string,
 );
+
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const characters = await client.query(api.characters.listBackend, {});
   const images = await client.query(api.public.listImages, {});
 
+  // Define your website's base URL once
+const baseUrl = process.env.NEXTAUTH_URL || "https://my-ai-companion-docs.vercel.app/";
+
   return [
     {
-      url: "https://openroleplay.ai",
+      url: baseUrl,
       lastModified: new Date(),
       changeFrequency: "daily",
       priority: 1,
     },
     {
-      url: "https://openroleplay.ai/models",
+      url: `${baseUrl}/models`,
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 0.8,
     },
     {
-      url: "https://openroleplay.ai/crystals",
+      url: `${baseUrl}/crystals`,
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 0.8,
     },
     ...characters.map((character) => ({
-      url: `https://openroleplay.ai/character/${character._id}`,
+      url: `${baseUrl}/character/${character._id}`,
       lastModified: new Date(character.updatedAt),
       changeFrequency: "daily" as "daily",
       priority: 0.8,
     })),
     ...images.map((image) => ({
-      url: `https://openroleplay.ai/image/${image._id}`,
+      url: `${baseUrl}/image/${image._id}`,
       lastModified: new Date(image._creationTime),
       changeFrequency: "daily" as "daily",
       priority: 0.8,
